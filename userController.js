fbe5311e9945d350db45d68ba69c7559f9a639ec
gp437 <56308971+gp437@users.noreply.github.com>
2020-01-31 06:42:54 +0000

    user api model

---
+++ userController.js
@@ -0,0 +1,98 @@
+// contactController.js
+// Import contact model
+User = require('./userModel');
+// Handle index actions
+exports.index = function (req, res) {
+    User.get(function (err, users) {
+        if (err) {
+            res.json({
+                status: "error",
+                message: err,
+            });
+        }
+        res.json({
+            status: "success",
+            message: "users retrieved successfully",
+            data: users
+        });
+    });
+};
+// Handle create contact actions
+exports.new = function (req, res) {
+    var user = new User();
+    user.name = req.body.name ? req.body.name : user.name;
+    user.gender = req.body.gender;
+    user.email = req.body.email;
+    user.phone = req.body.phone;
+    user.userType = req.body.userType;
+        user.password = req.body.password;
+// save the contact and check for errors
+    user.save(function (err) {
+res.json({
+            message: 'New user created!',
+            data: user
+        });
+    });
+};
+// Handle view user info
+exports.view = function (req, res) {
+    User.findById(req.params.user_id, function (err, user) {
+        if (err)
+            res.send(err);
+        res.json({
+            message: 'user details loading..',
+            data: user
+        });
+    });
+};
+
+//get email info
+exports.view = function (req, res) {
+    let email = req.params.email
+    User.find( {"email":  email }, function (err, user) {
+        if (err)
+            res.send(err);
+        res.json({
+            message: 'user details loading..',
+            data: user
+        });
+    });
+};
+
+
+
+// Handle update contact info
+exports.update = function (req, res) {
+User.findById(req.params.user_id, function (err, user) {
+        if (err)
+            res.send(err);
+user.name = req.body.name ? req.body.name : user.name;
+        user.gender = req.body.gender;
+        user.email = req.body.email;
+        user.phone = req.body.phone;
+        user.userType = req.body.userType;
+        user.password = req.body.password;
+// save the contact and check for errors
+        user.save(function (err) {
+            if (err)
+                res.json(err);
+            res.json({
+                message: 'Contact Info updated',
+                data: user
+            });
+        });
+    });
+};
+// Handle delete user
+exports.delete = function (req, res) {
+    User.remove({
+        _id: req.params.user_id
+    }, function (err, user) {
+        if (err)
+            res.send(err);
+res.json({
+            status: "success",
+            message: 'user deleted'
+        });
+    });
+};
---
+++ userModel.js
@@ -0,0 +1,32 @@
+// contactModel.js
+var mongoose = require('mongoose');
+// Setup schema
+var userSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    userType: {
+        type : String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    gender: String,
+    phone: String,
+    create_date: {
+        type: Date,
+        default: Date.now
+    }
+});
+// Export Contact model
+var User = module.exports = mongoose.model('user', userSchema);
+module.exports.get = function (callback, limit) {
+    User.find(callback).limit(limit);
+}
